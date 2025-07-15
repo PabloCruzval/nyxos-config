a08cd5fb30f223d8b4850e3582c218f6c40f2790
PabloCruzval <p.cruzat.valenzuela@gmail.com>
2025-07-15 15:16:13 -0400

    Add some alias

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -52,7 +52,7 @@
 				# Git aliases
 				gs = "git status";
 				ga = "git add";
-				gc = "git commit";
+				gc = "git commit -m";
 				gp = "git push";
 				gl = "git pull";
 				gd = "git diff";
@@ -65,6 +65,7 @@
 				nyx-pcmasterrace = "sudo nixos-rebuild switch --flake /home/nyx/nixos-config/#nyx-pcmasterrace";
 				nyx-base = "sudo nixos-rebuild switch --flake /home/nyx/nixos-config/#nixos";
 				nyx-update = "cd /home/nyx/nixos-config && nix flake update";
+				npm-dev="nix-shell -p glib --run 'npm run dev'"
 			};
 
 			# Initialize zoxide and configure powerlevel10k
